ef23972df637e7315bbece41c192d76ed075f871
bangdler <zbthz90@gmail.com>
2022-05-28 03:12:36 +0900

    feat: 이전, 다음 버튼 컴포넌트 및 클릭 기능 구현

---
+++ airbnb-app/src/component/calender/PrevButton.jsx
@@ -0,0 +1,26 @@
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import styled from 'styled-components';
+
+function PrevButton({ curDate, setCurDate }) {
+  function handleClick() {
+    const newDate =
+      curDate.month === 1 ? { year: curDate.year - 1, month: 12 } : { year: curDate.year, month: curDate.month - 1 };
+    setCurDate(newDate);
+  }
+
+  return (
+    <StyledDiv>
+      <ArrowBackIosIcon onClick={handleClick} sx={{ fontSize: 24, cursor: 'pointer' }} />
+    </StyledDiv>
+  );
+}
+
+const StyledDiv = styled.div`
+  position: absolute;
+  left: 88px;
+  top: 64px;
+  //left: 16%;
+  //top: 13%;
+`;
+
+export default PrevButton;
