432f82e3b8f449c839f963225a133cee2c7aa60c
moonyerim2 <moonyerim2@gmail.com>
2022-05-31 23:33:23 +0900

    refactor: ModalContainer 스타일 theme 사용 하도록 수정

--- airbnb-app/src/styled-component/ModalContainer.js
+++ airbnb-app/src/styled-component/ModalContainer.js
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 
 export const ModalContainer = styled.div`
   position: relative;
+  padding: 40px;
   margin: 30px auto 0;
-  background-color: white;
-  border-radius: ${({ theme }) => theme.borderRadius.radius2};
-  box-shadow: 0 4px 10px rgba(51, 51, 51, 0.1), 0 0 4px rgba(51, 51, 51, 0.05);
+  background-color: ${({ theme }) => theme.modal.backgourndColor};
+  border-radius: ${({ theme }) => theme.modal.borderRadius};
+  box-shadow: ${({ theme }) => theme.modal.boxShadow};
 `;
