811f4262d2820e7868dae55b7159beef69cb2807
moon-yerim <75062526+moonyerim2@users.noreply.github.com>
2022-06-02 10:53:17 +0900

    Feature/11 (#45)
    * feat: 성인 없이 어린이, 유아 인원 추가 방지 기능 추가
    
    * fix: 성인 인원을 줄일 수 없는 상황에 버튼 비활성화 색상 스타일 적용
    
    * refactor: ResetButton 컴포넌트 onClick 핸들러 props로 받도록 수정
    
    * feat: 인원 설정 초기화 기능 추가
    
    * refactor&style: 중복코드 삭제, 줄띄움
    
    * fix: 검색바 초기화 버튼 유무따라 크기가 변경되는 부분 수정
    - 검색 버튼 아이콘 컬러 변경

--- airbnb-app/src/context/PersonnelProvider.jsx
+++ airbnb-app/src/context/PersonnelProvider.jsx
@@ -1,9 +1,11 @@
 import { createContext, useState } from 'react';
 import { PERSONNEL_STATE_KEY, PERSONNEL_MIN as MIN, PERSONNEL_MAX as MAX } from '@/constants/PersonnelText';
 
-const states = Object.values(PERSONNEL_STATE_KEY);
+const { ADULT, CHILD, INFANT } = PERSONNEL_STATE_KEY;
+const stateKeys = Object.values(PERSONNEL_STATE_KEY);
+
 const initialPersonnel = (() => {
-  return states.reduce((initialPersonnel, state) => {
+  return stateKeys.reduce((initialPersonnel, state) => {
     initialPersonnel[state] = 0;
     return initialPersonnel;
   }, {});
@@ -14,29 +16,52 @@ const PersonnelContext = createContext({});
 function PersonnelProvider({ children }) {
   const [personnel, setPersonnel] = useState(initialPersonnel);
 
-  const addPerson = title => {
-    if (personnel[title] === MAX) return;
-
+  const addAdultTogether = title => {
     setPersonnel(prevState => {
-      return { ...prevState, [title]: prevState[title] + 1 };
+      return { ...prevState, [ADULT]: prevState[title] + 1, [title]: prevState[title] + 1 };
     });
   };
 
+  const isNoAdult = title => personnel[ADULT] === 0 && (title === CHILD || title === INFANT);
+
+  const addPerson = title => {
+    if (personnel[title] === MAX) return;
+
+    if (isNoAdult(title)) {
+      addAdultTogether(title);
+    } else {
+      setPersonnel(prevState => {
+        return { ...prevState, [title]: prevState[title] + 1 };
+      });
+    }
+  };
+
+  const canNotRemoveAdult = title =>
+    title === ADULT && personnel[ADULT] === 1 && (personnel[CHILD] || personnel[INFANT]);
+
   const removePerson = title => {
     if (personnel[title] === MIN) return;
+    if (canNotRemoveAdult(title)) return;
 
     setPersonnel(prevState => {
       return { ...prevState, [title]: prevState[title] - 1 };
     });
   };
 
+  const resetPersonnel = () => {
+    setPersonnel(initialPersonnel);
+  };
+
   const personnelValue = (() => {
-    const { ADULT, CHILD, INFANT } = PERSONNEL_STATE_KEY;
+    if (personnel[ADULT] + personnel[CHILD] + personnel[INFANT] === 0) return '';
+
     return `게스트${personnel[ADULT] + personnel[CHILD]} 명, 유아${personnel[INFANT]} 명`;
   })();
 
   return (
-    <PersonnelContext.Provider value={{ personnel, personnelValue, addPerson, removePerson }}>
+    <PersonnelContext.Provider
+      value={{ personnel, personnelValue, addPerson, removePerson, canNotRemoveAdult, resetPersonnel }}
+    >
       {children}
     </PersonnelContext.Provider>
   );
