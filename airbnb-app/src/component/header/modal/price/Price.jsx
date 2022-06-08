7252669ce5d22175d3e256c7a30a40ad6e41f226
moonyerim2 <moonyerim2@gmail.com>
2022-06-08 17:54:39 +0900

    feat: 요금 설정 모달 요금별 데이터 그래프 추가 - 임시 데이터 추가

--- airbnb-app/src/component/header/modal/price/Price.jsx
+++ airbnb-app/src/component/header/modal/price/Price.jsx
@@ -1,12 +1,55 @@
-import styled from 'styled-components';
+import styled, { useTheme } from 'styled-components';
 import { ModalContainer } from '@/styled-component/ModalContainer';
 import { Title } from '@/styled-component/Title';
+import { Chart as ChartJS, registerables } from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { priceData } from '@/mock-data/priceData';
+
+ChartJS.register(...registerables);
+
+const priceGraphData = Object.entries(priceData).map(key => {
+  return { x: key[0], y: key[1] };
+});
 
 function Price() {
+  const theme = useTheme();
+
+  const data = {
+    datasets: [
+      {
+        data: priceGraphData,
+        borderColor: 'transparent',
+        backgroundColor: theme.color.black,
+        fill: true,
+        lineTension: 0.5,
+        pointBackgroundColor: 'transparent',
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    scales: {
+      x: {
+        display: false,
+      },
+      y: {
+        display: false,
+      },
+    },
+  };
+
   return (
-    <Container>
-      <Title>가격 범위</Title>
-    </Container>
+    <>
+      <Container>
+        <Title>가격 범위</Title>
+        <Line type="line" height={'150px'} data={data} options={options} />
+      </Container>
+    </>
   );
 }
 
